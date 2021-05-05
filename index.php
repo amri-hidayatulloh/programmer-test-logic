a4d25e58cacae196911718dcf474054b914a98fa
amrihidayatulloh <amri.hidayatulloh@gmail.com>
2021-05-06 01:52:06 +0700

    Adding some test cases

--- index.php
+++ index.php
@@ -17,4 +17,10 @@ function positivePairs($arr = []) {
 }
 
 $datatest = positivePairs([1, -1, 3, -4, 5, -2, 7, 4, 2]);
-print_r($datatest);
+print_r($datatest); //[1,2,4]
+
+$datatest = positivePairs([5, -2, -6, -1, 5, 1, 7, 4, 7]);
+print_r($datatest); //[1,5,7]
+
+$datatest = positivePairs([100, 30, -20, -5, -30, 20, 10, 50, -70, 80, 2, -2, -50]);
+print_r($datatest); //[2,20,30,50]
